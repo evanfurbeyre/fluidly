26f1267405e91311f2395809bf11a86aafa92c75
nate f <nmfurbearr@outlook.com>
2022-10-27 13:09:46 -0700

    swap to mp4

--- src/components/Audio.tsx
+++ src/components/Audio.tsx
@@ -9,7 +9,7 @@ const Audio = (props: Props) => {
   return (
     <div className="flex">
       <audio className="w-full rounded-2xl" controls>
-        <source src={src} type="audio/wav" />
+        <source src={src} type="audio/mp4" />
       </audio>
       {/* {session && withOptions && (
         <div className="flex w-12 grow-0 items-center justify-center">
--- src/components/AudioInput.tsx
+++ src/components/AudioInput.tsx
@@ -33,7 +33,7 @@ const AudioInput = ({ onSubmit, onCancel }: Props) => {
     if (!recorder) return null
     recorder.ondataavailable = (e) => chunks.push(e.data)
     recorder.onstop = async () => {
-      blob = new Blob(chunks, { type: "audio/wav" })
+      blob = new Blob(chunks, { type: "audio/mp4" })
       setAudioURL(window.URL.createObjectURL(blob))
       setRecording(false)
     }
