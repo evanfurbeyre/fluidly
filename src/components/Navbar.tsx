0a523db842275340311a59c4ef56f20e1b88b18f
Evan Furbeyre <efurbeyre@redzone.co>
2022-10-28 14:39:14 -0700

    get rid of admin page in favor of putting things on index

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,16 @@
-import { useSession } from "next-auth/react"
 import Link from "next/link"
 import { useContext } from "react"
 import { AdminContext } from "../pages/_app"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const NavBar = () => {
-  const { data: authed } = useSession()
+  const { data: session } = useSession()
   const { adminMode, setAdminMode } = useContext(AdminContext)
 
   return (
     <div className="navbar sticky top-0 border-b-2 bg-base-100">
       <div className="navbar-start">
-        {authed && (
+        {session && (
           <div className="pl-2">
             <Link href={"/admin"}>Admin Page</Link>
           </div>
@@ -19,11 +19,11 @@ const NavBar = () => {
       <div className="navbar-center">
         <a className="text-xl font-semibold normal-case">fluidly</a>
       </div>
-      <div className="navbar-end">
-        {authed && (
+      <div className="navbar-end mr-6">
+        {session && (
           <div className="form-control grow-0">
             <label className="label cursor-pointer">
-              <span className="label-text mr-4">Admin?</span>
+              <span className="label-text mr-2">Admin?</span>
               <input
                 type="checkbox"
                 className="toggle-primary toggle toggle-sm"
@@ -34,6 +34,13 @@ const NavBar = () => {
           </div>
         )}
       </div>
+      <button
+        type="button"
+        className="btn-outline btn-sm btn mr-4"
+        onClick={session ? () => signOut() : () => signIn()}
+      >
+        {session ? "Sign out" : "Sign in"}
+      </button>
     </div>
   )
 }
