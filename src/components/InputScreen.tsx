b05307ef462816d2f19c3c0c741d38823f345cd7
nate f <nmfurbearr@outlook.com>
2022-11-03 13:32:41 -0700

    add native language tooltip to submission page

--- src/components/InputScreen.tsx
+++ src/components/InputScreen.tsx
@@ -1,3 +1,4 @@
+import { InformationCircleIcon } from "@heroicons/react/24/solid"
 import { Language } from "@prisma/client"
 import { trpc } from "../utils/trpc"
 import { ResponseWithRelations } from "../utils/types"
@@ -13,6 +14,7 @@ const InputScreen = (props: Props) => {
   const { response, refetchResponse } = props
   const addResponseAudio = trpc.response.addResponseAudio.useMutation()
   const targetLang = response.user.targetLang as Language
+  const nativeLang = response.user.nativeLang as Language
 
   const submitResponseAudio = async (key: string) => {
     addResponseAudio.mutate(
@@ -30,7 +32,12 @@ const InputScreen = (props: Props) => {
   return (
     <div className="flex h-[calc(100vh-12rem)] flex-col items-center justify-center">
       <div className="flex max-w-md flex-col gap-12">
-        <h1 className="text-center text-2xl">{response.prompt[targetLang]}</h1>
+        <h1 className="text-center text-2xl">
+          <span className="tooltip" data-tip={response.prompt[nativeLang]}>
+            <InformationCircleIcon className="float-right h-6 w-6 fill-gray-400 pb-2" />
+            {response.prompt[targetLang]}
+          </span>
+        </h1>
         {response.audio?.audioUrl && (
           <>
             <Audio src={response.audio.audioUrl} />
